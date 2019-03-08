92c4b95872ee0f73f2dae2d903abe0a2aa893fdb
tomatillodesign <chris@tomatillodesign.com>
2019-03-08 09:57:46 -0500

    Updates

---
+++ precache-manifest.3ff25c3652ad03c21686e8c4d5387dbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "218a330773f778b66bd6",
+    "url": "/the-unscrambler/static/css/main.002edca0.chunk.css"
+  },
+  {
+    "revision": "218a330773f778b66bd6",
+    "url": "/the-unscrambler/static/js/main.95b96754.chunk.js"
+  },
+  {
+    "revision": "ab82b72fb06da145046d",
+    "url": "/the-unscrambler/static/js/runtime~main.4a693899.js"
+  },
+  {
+    "revision": "bb1cfbc77e0591448a8c",
+    "url": "/the-unscrambler/static/js/2.ec62849d.chunk.js"
+  },
+  {
+    "revision": "c67624f9d1c0ad085666f178f22a504f",
+    "url": "/the-unscrambler/index.html"
+  }
+];
